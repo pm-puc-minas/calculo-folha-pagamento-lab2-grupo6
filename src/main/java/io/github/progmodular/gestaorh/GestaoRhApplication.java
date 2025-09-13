0debd9570280e3a083fd1860f72b759c77849582
Matheus Barbosa <augustomatheus1206@gmail.com>
2025-09-13 02:20:35 -0300

    Esqueleto spring criado!

--- src/main/java/io/github/progmodular/gestaorh/GestaoRhApplication.java
+++ src/main/java/io/github/progmodular/gestaorh/GestaoRhApplication.java
@@ -2,9 +2,18 @@ package io.github.progmodular.gestaorh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GestaoRhApplication {
+
+    @GetMapping("/hello-world")
+    public String helloWorld()
+    {
+        return "Hello World";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestaoRhApplication.class, args);
