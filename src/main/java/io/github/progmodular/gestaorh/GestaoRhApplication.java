4e6923a8a654658102a49d9ac68caffa336c2601
Matheus Barbosa <augustomatheus1206@gmail.com>
2025-09-13 02:08:17 -0300

    ok

---
+++ src/main/java/io/github/progmodular/gestaorh/GestaoRhApplication.java
@@ -0,0 +1,13 @@
+package io.github.progmodular.gestaorh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoRhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoRhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/progmodular/gestaorh/GestaoRhApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.progmodular.gestaorh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoRhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
