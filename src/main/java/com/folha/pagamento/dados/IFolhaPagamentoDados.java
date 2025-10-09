47c376e351e7ac85e01bf96a965a3833e35becfb
Sofia Melo Duque <sofisduque2004@gmail.com>
2025-10-09 17:42:22 -0300

    sprint2.0 parte inicial sem reajustes

---
+++ src/main/java/com/folha/pagamento/dados/IFolhaPagamentoDados.java
@@ -0,0 +1,21 @@
+package com.folha.pagamento.dados;
+
+import java.math.BigDecimal;
+
+public interface IFolhaPagamentoDados {
+
+    // RF10 e RFs de Funcionario
+    String getNomeFuncionario();
+    String getCargoFuncionario();
+
+    // Proventos e Base
+    BigDecimal getSalarioBruto();
+
+    // Descontos (usando BigDecimal - Requisito Extra)
+    BigDecimal getDescontoINSS();
+    BigDecimal getDescontoIRRF();
+    BigDecimal getTotalDescontos();
+
+    // Resultado Final
+    BigDecimal getSalarioLiquido();
+}
