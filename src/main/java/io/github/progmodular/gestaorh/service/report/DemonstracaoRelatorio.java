ef49488c4ec6b15f07c233399e19b1895d4be5cf
sofiameloo <130319798+sofiameloo@users.noreply.github.com>
2025-10-11 20:20:54 -0300

    Update DemonstracaoRelatorio.java

--- src/main/java/io/github/progmodular/gestaorh/service/report/DemonstracaoRelatorio.java
+++ src/main/java/io/github/progmodular/gestaorh/service/report/DemonstracaoRelatorio.java
@@ -4,26 +4,15 @@ import io.github.progmodular.gestaorh.service.payment.dados.IFolhaPagamentoDados
 
 import java.math.BigDecimal;
 
-/**
-
- */
 class MainDadosMock implements IFolhaPagamentoDados {
-    // Dados Fixos de Exemplo (RF10)
     @Override public String getNomeFuncionario() { return "João do Relatório"; }
     @Override public String getCargoFuncionario() { return "Analista de Sistemas"; }
     @Override public BigDecimal getSalarioBruto() { return new BigDecimal("7507.49"); }
-
-    // Dados Calculados (Que foram testados)
     @Override public BigDecimal getDescontoINSS() { return new BigDecimal("877.24"); }
     @Override public BigDecimal getDescontoIRRF() { return new BigDecimal("849.68"); }
     @Override public BigDecimal getTotalDescontos() { return new BigDecimal("1726.92"); }
     @Override public BigDecimal getSalarioLiquido() { return new BigDecimal("5780.57"); }
 }
-
-/**
- *
- *(Main) para demonstrar a execução do (Relatório).
- */
 public class DemonstracaoRelatorio {
 
     public static void main(String[] args) {
