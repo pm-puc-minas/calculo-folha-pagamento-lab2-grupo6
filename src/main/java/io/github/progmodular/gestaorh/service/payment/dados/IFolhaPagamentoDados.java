a19911daee1befac70ec26d03ebf1db746bc61b1
sofiameloo <130319798+sofiameloo@users.noreply.github.com>
2025-10-11 20:16:13 -0300

    Update IFolhaPagamentoDados.java

--- src/main/java/io/github/progmodular/gestaorh/service/payment/dados/IFolhaPagamentoDados.java
+++ src/main/java/io/github/progmodular/gestaorh/service/payment/dados/IFolhaPagamentoDados.java
@@ -4,18 +4,14 @@ import java.math.BigDecimal;
 
 public interface IFolhaPagamentoDados {
 
-    // RF10 e RFs de Funcionario
     String getNomeFuncionario();
     String getCargoFuncionario();
 
-    // Proventos e Base
     BigDecimal getSalarioBruto();
 
-    // Descontos (usando BigDecimal - Requisito Extra)
     BigDecimal getDescontoINSS();
     BigDecimal getDescontoIRRF();
     BigDecimal getTotalDescontos();
 
-    // Resultado Final
     BigDecimal getSalarioLiquido();
 }
